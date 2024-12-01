2633917091f9a6a3a6d4f3fc035e59b7f89ffed4
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2024-12-01 17:44:03 +0800

    here i am

--- homeharmonix/settings.py
+++ homeharmonix/settings.py
@@ -149,8 +149,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
